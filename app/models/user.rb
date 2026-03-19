5356cafb4de21345af77e8cdc345554ab4db6795
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-03-19 15:07:07 +0900

    ユーザー登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, uniqueness: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
 end
