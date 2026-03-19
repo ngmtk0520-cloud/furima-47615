7ee8f4a692c59021e95a06204a6f80974caa6ea8
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-03-19 15:55:35 +0900

    ユーザーモデル、バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
 
-  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, uniqueness: true
-  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, uniqueness: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 },
+                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :nickname, :birth_date, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
