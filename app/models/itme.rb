31b1612a5d16d929f72e5ac01aa25184223b6ccc
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-03-20 15:51:01 +0900

    Itemsモデルの実装

--- app/models/itme.rb
+++ app/models/itme.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :sales_status
+  belongs_to :scheduled_delivery
+  belongs_to :shipping_fee_status
+  has_one_attached :image
+  validates :name, :info, :price, :image, presence: true
+  validates :category_id, :prefecture_id, :sales_status_id, :scheduled_delivery_id, :shipping_fee_status_id,
+            numericality: { other_than: 1 }
+  validates :price, numericality: {
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999
+  }
 end
