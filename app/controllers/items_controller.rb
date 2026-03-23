77c22098d5754a0bfc7c5c411a0b523b284d081d
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-03-23 16:09:09 +0900

    コントローラー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -17,10 +19,31 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :info, :price, :category_id, :prefecture_id, :sales_status_id, :scheduled_delivery_id,
                                  :shipping_fee_status_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    return unless current_user != @item.user
+
+    redirect_to root_path
   end
 end
